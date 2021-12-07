932a1ed970e8a74617e55980b431aa1ba30a6da0
David Reed <d.reed@salesforce.com>
2021-12-07 13:20:31 -0700

    Math up

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,32 +1,34 @@
-fn fuel_cost(crabs: &Vec<usize>, position: usize) -> usize {
-    crabs
-        .iter()
-        .map(|c| (*c as i64 - position as i64).abs() as usize)
-        .sum()
+fn fuel_cost(crabs: &Vec<i64>, position: i64) -> i64 {
+    crabs.iter().map(|c| (c - position).abs()).sum()
 }
 
-fn fuel_cost_for_move(crab: usize, position: usize) -> usize {
-    let diff = (crab as i64 - position as i64).abs() as usize;
-    (1..=diff).sum()
+fn fuel_cost_for_move(crab: i64, position: i64) -> i64 {
+    let diff = (crab - position).abs();
+    diff * (diff + 1) / 2
 }
 
-fn fuel_cost_part_2(crabs: &Vec<usize>, position: usize) -> usize {
+fn fuel_cost_part_2(crabs: &Vec<i64>, position: i64) -> i64 {
     crabs.iter().map(|c| fuel_cost_for_move(*c, position)).sum()
 }
 
 fn main() {
-    let crabs: Vec<usize> = include_str!("input.txt")
+    let crabs: Vec<i64> = include_str!("input.txt")
         .split(",")
-        .map(|s| s.parse::<usize>().unwrap())
+        .map(|s| s.parse::<i64>().unwrap())
         .collect();
+
+    let furthest_crab = *crabs.iter().max().unwrap();
 
     println!(
         "Cheapest position: {}",
-        (0..1000).map(|p| fuel_cost(&crabs, p)).min().unwrap()
+        (0..furthest_crab)
+            .map(|p| fuel_cost(&crabs, p))
+            .min()
+            .unwrap()
     );
     println!(
         "Cheapest position, part 2: {}",
-        (0..1000)
+        (0..furthest_crab)
             .map(|p| fuel_cost_part_2(&crabs, p))
             .min()
             .unwrap()
